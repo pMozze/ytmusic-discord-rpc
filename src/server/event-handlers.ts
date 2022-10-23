a4a322fc0eeea107db1c5ad4c84ec3664548347c
pMozze <ikMozze@gmail.com>
2022-10-23 19:13:01 +0300

    upd

--- src/server/event-handlers.ts
+++ src/server/event-handlers.ts
@@ -3,48 +3,62 @@ import { WebSocket } from 'ws';
 import * as DiscordRPC from 'discord-rpc';
 import IMetaData from '../models/IMetaData';
 
-const discord = {
-	client: null,
-	clientId: '962400209533546566',
-	isAuthorized: false
+interface IDiscord {
+  client: DiscordRPC.Client;
+  clientId: string;
+  isAuthorized: boolean;
+};
+
+const discord: IDiscord = {
+  client: null,
+  clientId: '942539762227630162',
+  isAuthorized: false
 };
 
 const onClientConnected = async (webSocketClient: WebSocket) => {
-	discord.client = new DiscordRPC.Client({ transport: 'ipc' });
-	discord.client.login({ clientId: discord.clientId }).then(() => {
-		discord.isAuthorized = true;
-	}).catch((error: any) => {
-		console.error('An error accured when trying discord login.', error);
-		webSocketClient.close();
-	});
+  discord.client = new DiscordRPC.Client({ transport: 'ipc' });
+
+  try {
+    await discord.client.login({ clientId: discord.clientId });
+    discord.isAuthorized = true;
+  } catch (error) {
+    console.error('An error accured when trying discord login.', error);
+    webSocketClient.close();
+  }
 };
 
-const onDataRecieved = (metadata: IMetaData) => {
-	if (!discord.isAuthorized) {
-		return;
-	}
+const onDataRecieved = async (metadata: IMetaData) => {
+  if (!discord.isAuthorized) {
+    return;
+  }
 
-	discord.client.setActivity({
-		state: metadata.artist,
-		details: metadata.title,
-		largeImageKey: metadata.artwork,
-		buttons: [{
-			label: 'Я люблю Златочку ❤️',
-			url: 'https://music.youtube.com/'
-		}]
-	}).catch((error: any) => {
-		console.error('An error accured when trying set discord activity.', error);
-	});
+  try {
+    await discord.client.setActivity({
+      state: metadata.artist,
+      details: metadata.title,
+      largeImageKey: metadata.artwork,
+      buttons: [{
+        label: 'Я люблю Златочку ❤️',
+        url: 'https://music.youtube.com/'
+      }]
+    });
+  } catch (error) {
+    console.error('An error accured when trying set discord activity.', error);
+  }
 };
 
 const onClientDisconnected = async () => {
-	discord.isAuthorized = false;
-	await discord.client.clearActivity();
-	await discord.client.destroy();
+  try {
+    discord.isAuthorized = false;
+    await discord.client.clearActivity();
+    await discord.client.destroy();
+  } catch (error) {
+    console.error('An error accured when destroying discrod client.', error);
+  }
 };
 
 export {
-	onClientConnected,
-	onDataRecieved,
-	onClientDisconnected
+  onClientConnected,
+  onDataRecieved,
+  onClientDisconnected
 };
--- src/server/index.ts
+++ src/server/index.ts
@@ -5,28 +5,33 @@ const webSocketServer = new WebSocketServer({ port: 1337 });
 console.log(`WebSocket server is started. Port: ${webSocketServer.options.port}`);
 
 webSocketServer.on('connection', webSocketClient => {
-	if (webSocketServer.clients.size > 1) {
-		webSocketClient.close();
-		return;
-	}
+  if (webSocketServer.clients.size > 1) {
+    webSocketClient.close();
+    return;
+  }
 
-	console.log('Web extension client has connected');
-	onClientConnected(webSocketClient);
+  console.log('Web extension client has connected');
+  onClientConnected(webSocketClient);
 
-	webSocketClient.on('message', data => {
-		onDataRecieved(JSON.parse(data.toString()));
-	});
+  webSocketClient.on('message', data => {
+    try {
+      const jsonData = JSON.parse(data.toString());
+      onDataRecieved(jsonData);
+    } catch (error) {
+      console.error(error);
+    }
+  });
 
-	webSocketClient.on('close', () => {
-		onClientDisconnected();
-		console.log('Web extension client has disconnected');
-	});
+  webSocketClient.on('close', () => {
+    onClientDisconnected();
+    console.log('Web extension client has disconnected');
+  });
 });
 
 webSocketServer.on('error', error => {
-	console.error('WebSocket server is accured error:', error);
+  console.error('WebSocket server is accured error:', error);
 });
 
 webSocketServer.on('close', () => {
-	console.log('WebSocket server is closed');
+  console.log('WebSocket server is closed');
 });
